9e73b8da4df34cc12b5d673a952aba5bc28beafb
Holt Picklesimer <holtpicklesimer@gmail.com>
2021-02-10 13:09:18 -0700

    Added Routing.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,24 +11,24 @@ export class RecipeService {
       'A Test Recipe',
       'This is a test.',
       'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-      ]),
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+    ),
     new Recipe(
       'Another Test Recipe',
       'This is a test.',
       'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872',
-      [
-        new Ingredient('Buns', 2),
-        new Ingredient('Meat', 1)
-      ])
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
+    ),
   ];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
